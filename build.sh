1f8762d9132af1fb99fcfef577dd1fef014c8bb0
CiriloRibeiro <cirilo.ribeiroluiz@gmail.com>
2024-04-14 20:11:24 -0300

    last

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-cd backend
-
+# Convert static asset files
 python manage.py collectstatic --no-input
-python manage.py makemigrations
+
+# Apply any outstanding database migrations
 python manage.py migrate
