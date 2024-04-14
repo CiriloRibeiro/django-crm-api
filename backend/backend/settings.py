4ed0ee491f7ca227084ae5e2e5ca3a0478f54ab5
CiriloRibeiro <cirilo.ribeiroluiz@gmail.com>
2024-04-14 19:14:23 -0300

    las

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,11 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 
-from django.core.management.utils import get_random_secret_key
-
 import dj_database_url
 
+from django.core.management.utils import get_random_secret_key
+
 SECRET_KEY = get_random_secret_key()
+
 
 DEBUG = False
 
